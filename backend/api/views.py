d93077357ed7a6d77c3657973fe5b884f246f0c9
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-09-01 20:40:18 +0500

    fixed some issues with create records

--- backend/api/views.py
+++ backend/api/views.py
@@ -155,7 +155,7 @@ def createEnvironmentalParameters(request):
         )
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+    print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -171,6 +171,7 @@ def updateEnvironmentalParameters(request, pk):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
+    print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
